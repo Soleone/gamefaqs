fe0fe0eaa24489006fd78c6aeb6a088af8eb0723
soleone@gmail.com <Soleone@.(none)>
2008-11-14 05:59:31 +0100

    Random library is a bit more accessible now.

--- lib/gamefaqs/random.rb
+++ lib/gamefaqs/random.rb
@@ -4,13 +4,14 @@ module GameFaqs
     
     # A review for a game
     def review(game, platform)
-      game = Search.game(game, platform) if game.is_a?(String)
+      game = Search.game(game, platform=game.platform) if game.is_a?(String)
       game and game.reviews[rand(game.reviews.size)]
     end
     
-    def one_line_review(game, platform, options={:detailed => false})
+    def one_line_review(game, platform=game.platform, options={:detailed => false})
       rev = review(game, platform)
       rev and "#{options[:detailed] ? (rev.game.to_s << ': ') : ''}\"#{rev.title}\" - #{rev.score}"
     end
+    alias :opinion :one_line_review
   end
 end
